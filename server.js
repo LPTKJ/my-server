0beae8546f392cd5ebcc173d7d850df17bd26692
LPTKJ <943723339@qq.com>
2026-05-07 01:51:39 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+let cmd = '';
+
+app.get('/send', (req, res) => {
+    const c = req.query.c;
+    if (c) { cmd = c; res.send('OK: ' + c); }
+    else res.send('需要?c=指令');
+});
+
+app.get('/get', (req, res) => {
+    const tmp = cmd;
+    cmd = '';
+    res.send(tmp);
+});
+
+app.listen(3000, () => console.log('已启动'));
